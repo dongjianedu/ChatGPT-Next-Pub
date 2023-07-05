bba7136837671b6178305834883cd099059cc588
dongjianedu <deejac@qq.com>
2023-07-05 15:41:14 +0800

    Update auth.tsx
    修改认证页面

--- app/components/auth.tsx
+++ app/components/auth.tsx
@@ -7,6 +7,7 @@ import { useAccessStore } from "../store";
 import Locale from "../locales";
 
 import BotIcon from "../icons/bot.svg";
+import { Link } from "react-router-dom";
 
 export function AuthPage() {
   const navigate = useNavigate();
@@ -40,6 +41,9 @@ export function AuthPage() {
           onClick={goHome}
         />
         <IconButton text={Locale.Auth.Later} onClick={goHome} />
+        <Link to="https://pay.gpttool.tech/product/2.html">
+          <IconButton text={Locale.Auth.Buy} />
+        </Link>
       </div>
     </div>
   );
