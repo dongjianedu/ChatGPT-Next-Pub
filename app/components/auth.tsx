9ccc75507e656625e0d42556ed6eedb6a500f502
dongjianedu <deejac@qq.com>
2023-07-05 15:46:37 +0800

    Update auth.tsx
    修改认证页面

--- app/components/auth.tsx
+++ app/components/auth.tsx
@@ -22,7 +22,6 @@ export function AuthPage() {
       </div>
 
       <div className={styles["auth-title"]}>{Locale.Auth.Title}</div>
-      <div className={styles["auth-tips"]}>{Locale.Auth.Tips}</div>
 
       <input
         className={styles["auth-input"]}
